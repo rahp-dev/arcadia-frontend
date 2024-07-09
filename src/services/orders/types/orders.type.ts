9dca668409a996c7980ebda4e21997385b0016c4
rahp-dev <rahp.dev@gmail.com>
2024-07-09 17:08:40 -0400

    [UPDATE] corrigiendo order details

--- src/services/orders/types/orders.type.ts
+++ src/services/orders/types/orders.type.ts
@@ -7,6 +7,7 @@ export type Order = {
   numQuotes: number
   financed: boolean
   transactionDate: Date
+  tickets: Array<number>
   ticketIds: Array<number>
 }
 
--- src/views/orders/OrderDetails/OrderDetails.tsx
+++ src/views/orders/OrderDetails/OrderDetails.tsx
@@ -1,9 +1,14 @@
-import { Button, Card, Tabs } from '@/components/ui'
+import { Button, Card, Skeleton, Tabs } from '@/components/ui'
 import TabList from '@/components/ui/Tabs/TabList'
 import TabNav from '@/components/ui/Tabs/TabNav'
 import { useGetOrderByIdQuery } from '@/services/RtkQueryService'
+import { format } from 'date-fns'
 import { FaEdit } from 'react-icons/fa'
-import { HiOutlineArrowRight, HiOutlineClipboardCheck } from 'react-icons/hi'
+import {
+  HiOutlineArrowRight,
+  HiOutlineClipboardCheck,
+  HiOutlineTicket,
+} from 'react-icons/hi'
 import { useNavigate, useParams } from 'react-router-dom'
 
 const OrderDetails = () => {
@@ -14,16 +19,22 @@ const OrderDetails = () => {
     refetchOnMountOrArgChange: true,
   })
 
+  const formattedDate = (date: Date | null) => {
+    if (!date) return
+
+    return format(new Date(date), 'dd-MM-yyyy - h:mm:ss a')
+  }
+
   const cardFooter = (
-    <Button variant="solid" size="sm" icon={<FaEdit />}>
+    <Button variant="solid" size="sm" icon={<FaEdit />} block>
       Editar orden
     </Button>
   )
 
   return (
     <>
-      <div className="flex items-center justify-between mb-4">
-        <h3>Detalles de la Orden #</h3>
+      <div className="flex items-center justify-between mb-6">
+        <h3>Detalles de la orden</h3>
         <Button
           size="sm"
           variant="solid"
@@ -36,7 +47,119 @@ const OrderDetails = () => {
 
       <div className="container mx-auto h-full">
         <div className="flex flex-row gap-4">
-          <Card className="xl:max-w-[360px]" footer={cardFooter}></Card>
+          <Card className="w-[380px]" footer={cardFooter}>
+            <div className="grid gap-y-4 gap-x-4">
+              {isFetching ? (
+                <Skeleton />
+              ) : (
+                <h5 className="text-center text-slate-600">
+                  Orden N°{data?.id}
+                </h5>
+              )}
+
+              <div className="flex flex-col">
+                {isFetching ? (
+                  <Skeleton />
+                ) : (
+                  <>
+                    <span className="font-semibold">Monto total:</span>
+                    <span>{data?.amount}</span>
+                  </>
+                )}
+              </div>
+
+              <div className="flex flex-col">
+                {isFetching ? (
+                  <Skeleton />
+                ) : (
+                  <>
+                    <span className="font-semibold">¿Fue financiado?:</span>
+                    <span>{data?.financed ? 'Sí' : 'No'}</span>
+                  </>
+                )}
+              </div>
+
+              <div className="flex flex-col">
+                {isFetching ? (
+                  <Skeleton />
+                ) : (
+                  <>
+                    <span className="font-semibold">¿Cuántas cuotas?:</span>
+                    <span>
+                      {data?.numQuotes > 0 ? `${data?.numQuotes}` : 'Ninguna'}
+                    </span>
+                  </>
+                )}
+              </div>
+
+              <div className="flex flex-col">
+                {isFetching ? (
+                  <Skeleton />
+                ) : (
+                  <>
+                    <span className="font-semibold">Método de Pago:</span>
+                    <span>{data?.paymentMethod}</span>
+                  </>
+                )}
+              </div>
+
+              <div className="flex flex-col">
+                {isFetching ? (
+                  <Skeleton />
+                ) : (
+                  <>
+                    <span className="font-semibold">
+                      N° de Referencia del Pago:
+                    </span>
+                    <span>
+                      {data?.paymentReference
+                        ? `${data?.paymentReference}`
+                        : 'Sin referencia'}
+                    </span>
+                  </>
+                )}
+              </div>
+
+              <div className="flex flex-col">
+                {isFetching ? (
+                  <Skeleton />
+                ) : (
+                  <>
+                    <span className="font-semibold">Estatus de la Orden:</span>
+                    <span>{data?.status}</span>
+                  </>
+                )}
+              </div>
+
+              <div className="flex flex-col">
+                {isFetching ? (
+                  <Skeleton />
+                ) : (
+                  <>
+                    <span className="font-semibold">Cantidad de Tickets:</span>
+                    <div className="flex gap-2">
+                      {data?.tickets.map((ticket: any, index: number) => (
+                        <HiOutlineTicket key={ticket.id} className="text-2xl" />
+                      ))}
+                    </div>
+                  </>
+                )}
+              </div>
+
+              <div className="flex flex-col">
+                {isFetching ? (
+                  <Skeleton />
+                ) : (
+                  <>
+                    <span className="font-semibold">
+                      Fecha de la Transacción:
+                    </span>
+                    <span>{formattedDate(data?.transactionDate)}</span>
+                  </>
+                )}
+              </div>
+            </div>
+          </Card>
           <Card className="w-full">
             <Tabs>
               <TabList>
