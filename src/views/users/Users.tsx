3bafa8b0bfc9d349207b9b01bd49a43c28f61432
rahp-dev <rahp.dev@gmail.com>
2024-07-25 16:49:19 -0400

    [UPDATE] tabla de usuarios fix

--- src/views/users/Users.tsx
+++ src/views/users/Users.tsx
@@ -215,7 +215,7 @@ const Users = () => {
           ))}
         </THead>
         {isFetching ? (
-          <TableRowSkeleton columns={5} rows={pageSize} />
+          <TableRowSkeleton columns={7} rows={pageSize} />
         ) : (
           <TBody>
             {table.getRowModel().rows.map((row) => {
